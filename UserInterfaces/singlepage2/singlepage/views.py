e35aed3839a261b39b74ee23c330cd463129fb27
kollabhargavre1 <kollabhargavreddy@gmail.com>
2023-01-17 22:55:59 +0530

    added userintergaces

---
+++ UserInterfaces/singlepage2/singlepage/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "singlepage/index.html")
+
+texts = ["Quantum computing is a rapidly-emerging technology that harnesses the laws of quantum mechanics to solve problems too complex for classical computers. Today, IBM Quantum makes real quantum hardware -- a tool scientists only began to imagine three decades ago -- available to hundreds of thousands of developers.",
+"ChatGPT is a chatbot launched by OpenAI in November 2022. It is built on top of OpenAI's GPT-3 family of large language models, and is fine-tuned with both supervised and reinforcement learning techniques.","OpenAI is an artificial intelligence research laboratory consisting of the for-profit corporation OpenAI LP and its parent company, the non-profit OpenAI Inc. The company conducts research in the field of AI with the stated goal of promoting and developing friendly AI in a way that benefits humanity as a whole"]
+
+
+def section(request,num):
+    if 1<=num<=3:
+        return HttpResponse(texts[num-1])
+    else:
+        raise Http404("No such section")
