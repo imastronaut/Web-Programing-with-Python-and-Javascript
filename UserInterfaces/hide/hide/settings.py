03afcedb484e5e5299dd4fdc2f234e5ab6a9d419
Kolla Bhargava Reddy <91125752+kollabhargavre@users.noreply.github.com>
2023-01-17 22:58:53 +0530

    added UserInterfaces

--- UserInterfaces/hide/hide/settings.py
+++ UserInterfaces/hide/hide/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b&$+b*!3%8zf2a2km3v_nvvcaz%kd)uq*448vt8f@hjpm9%d*j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
